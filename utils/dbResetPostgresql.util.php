64b2fe62b97c529c506e6ee939fa7f093d974913
pejisean <202311735@fit.edu.ph>
2025-06-24 05:01:13 +0800

    refactor - fixed composer.yaml to include the .env file hosted locally

--- utils/dbResetPostgresql.util.php
+++ utils/dbResetPostgresql.util.php
@@ -9,4 +9,30 @@
 
     // 3) envSetter
     require_once UTILS_PATH . '/envSetter.util.php';
+
+    // ——— Connect to PostgreSQL ———
+    $dsn = "pgsql:host={$pgConfig['pg_host']};port={$pgConfig['pg_port']};dbname={$pgConfig['pg_db']}";
+    $pdo = new PDO($dsn, $pgConfig['pg_user'], $pgConfig['pg_pass'], [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+
+    // Just indicator it was working
+    echo "Applying schema from database/user.model.sql…\n";
+
+    $sql = file_get_contents('database/user.model.sql');
+
+    // Another indicator but for failed creation
+    if ($sql === false) {
+    throw new RuntimeException("Could not read database/user.model.sql");
+    } else {
+        echo "Creation Success from the database/user.model.sql";
+    }
+
+    // If your model.sql contains a working command it will be executed
+    $pdo->exec($sql);
+
+    echo "Truncating tables…\n";
+    foreach (['users'] as $table) {
+    $pdo->exec("TRUNCATE TABLE {$table} RESTART IDENTITY CASCADE;");
+}
 ?>
--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,9 +1,19 @@
 <?php
+    require_once BASE_PATH . '/vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
     $dotenv->load();
 
     // Distribute the data using array key
-    $typeConfig = [
-        'key' => $_ENV['.env'],
+    $pgConfig = [
+        'pg_host'   => $_ENV['PG_HOST'] ?? null,
+        'pg_port'   => $_ENV['PG_PORT'] ?? null,
+        'pg_db'     => $_ENV['PG_DB'] ?? null,
+        'pg_user'   => $_ENV['PG_USER'] ?? null,
+        'pg_pass'   => $_ENV['PG_PASS'] ?? null,
     ];
+
+    $mongoConfig = [
+        'mongo_uri' => $_ENV['MONGO_URI'] ?? null,
+        'mongo_db'  => $_ENV['MONGO_DB'] ?? null,
+    ]
 ?>
