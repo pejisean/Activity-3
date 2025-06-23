bfd6c974896e3837be58bf4f9170b59ab030481c
pejisean <202311735@fit.edu.ph>
2025-06-23 10:00:09 +0800

    docs - did all the tasks for workbook_activity_3.md

---
+++ utils/dbResetPostgresql.util.php
@@ -0,0 +1,12 @@
+<?php
+    declare(strict_types=1);
+
+    // 1) Composer autoload
+    require 'vendor/autoload.php';
+
+    // 2) Composer bootstrap
+    require 'bootstrap.php';
+
+    // 3) envSetter
+    require_once UTILS_PATH . '/envSetter.util.php';
+?>
