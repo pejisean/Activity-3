186d6c3bc5697bc5d6bf7a787ad941f6ffebbdbb
pejisean <202311735@fit.edu.ph>
2025-07-08 21:32:30 +0800

    fix: update meeting_id type to SERIAL and add ON DELETE CASCADE to foreign keys in project_users table

--- database/meeting.model.sql
+++ database/meeting.model.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.meeting (
-    meeting_id int NOT NULL PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    description text,
-    start_time timestamptz NOT NULL,
-    end_time timestamptz NOT NULL,
-    location varchar
+    meeting_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ NOT NULL,
+    location VARCHAR(255)
 );
--- database/project_users.model.sql
+++ database/project_users.model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.project_users (
-    project_id INTEGER NOT NULL REFERENCES projects (id),
-    user_id INTEGER NOT NULL REFERENCES users (id),
+    project_id INTEGER NOT NULL REFERENCES public.projects (id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
     role VARCHAR(50) NOT NULL,
     PRIMARY KEY (project_id, user_id)
 );
