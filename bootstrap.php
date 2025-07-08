6588bd3942f97e982e2ba46c1e7f44c4032bdaf2
pejisean <202311735@fit.edu.ph>
2025-07-08 22:56:41 +0800

    fix: update DUMMIES_PATH to point to the correct static data directory

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', BASE_PATH.'/utils/');
-define('DUMMIES_PATH', BASE_PATH.'/database/');
+define('DUMMIES_PATH', BASE_PATH.'/staticDatas/');
 chdir(BASE_PATH);
