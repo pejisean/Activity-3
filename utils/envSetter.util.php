42984db606ab412beea1f255a68a610999365c01
pejisean <202311735@fit.edu.ph>
2025-07-08 16:24:03 +0800

    refactor - updated envSetter to use safeLoad and removed null coalescing for environment variables

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,19 +1,24 @@
 <?php
-    require_once BASE_PATH . '/vendor/autoload.php';
+     require_once BASE_PATH . '/vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
-    $dotenv->load();
+    $dotenv->safeLoad();
+
+    //populating getenv()
+    foreach ($_ENV as $key => $value) {
+    putenv("$key=$value");
+    }
 
     // Distribute the data using array key
     $pgConfig = [
-        'pg_host'   => $_ENV['PG_HOST'] ?? null,
-        'pg_port'   => $_ENV['PG_PORT'] ?? null,
-        'pg_db'     => $_ENV['PG_DB'] ?? null,
-        'pg_user'   => $_ENV['PG_USER'] ?? null,
-        'pg_pass'   => $_ENV['PG_PASS'] ?? null,
+        'pg_host'   => $_ENV['PG_HOST'],
+        'pg_port'   => $_ENV['PG_PORT'],
+        'pg_db'     => $_ENV['PG_DB'],
+        'pg_user'   => $_ENV['PG_USER'],
+        'pg_pass'   => $_ENV['PG_PASS'],
     ];
 
     $mongoConfig = [
-        'mongo_uri' => $_ENV['MONGO_URI'] ?? null,
-        'mongo_db'  => $_ENV['MONGO_DB'] ?? null,
+        'mongo_uri' => $_ENV['MONGO_URI'],
+        'mongo_db'  => $_ENV['MONGO_DB'],
     ]
 ?>
